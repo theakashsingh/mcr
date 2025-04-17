9ccbb2fd10e7ea8dcef3d25acdb0c21131c03d83
Akash Singh <mr.singhaksh@gmail.com>
2025-04-17 16:58:38 +0530

    FE:feat:add create folder/file and delte features

--- fileExplorer/src/App.jsx
+++ fileExplorer/src/App.jsx
@@ -1,14 +1,86 @@
-import './App.css'
-import FileExplorer from './component/fileExplorer'
-import data from './data.json'
+import { useState } from "react";
+import "./App.css";
+import FileExplorer from "./component/fileExplorer";
+import data from "./data.json";
 
 function App() {
+  const [file, setFile] = useState(data);
 
+  const handleCreateFile = (e, id, isFolder) => {
+    if(!e.target.value)return 
+    const newNode = {
+      id: String(Math.floor(Math.random() * 10000)),
+      name: e.target.value,
+      isFolder: isFolder,
+      children: isFolder ? [] : undefined,
+    };
+  
+    const updateNode = (nodeList) => {
+      return nodeList.map((node) => {
+        if (node.id === id) {
+          return {
+            ...node,
+            children: [...(node.children || []), newNode],
+          };
+        } else if (node.children && node.children.length > 0) {
+          return {
+            ...node,
+            children: updateNode(node.children),
+          };
+        }
+        return node;
+      });
+    };
+  
+    setFile((prev) => {
+      if (prev.id === id) {
+        // Root node match
+        return {
+          ...prev,
+          children: [...(prev.children || []), newNode],
+        };
+      }
+  
+      return {
+        ...prev,
+        children: updateNode(prev.children),
+      };
+    });
+  };
+  
+ 
+
+  const handleDeleteNode = (id) =>{
+    console.log('ID',id);
+    
+        const updateTree = (nodeList) =>{
+              return nodeList.filter(node=>node.id !== id).map((child)=>{
+                if (child.children) {
+                  return {...child,children:updateTree(child.children)}
+                }
+                return child
+              })
+        }
+        setFile((prev) => {
+          if (prev.id === id) {
+            // Root node match
+            return {};
+          }
+      
+          return {
+            ...prev,
+            children: updateTree(prev.children),
+          };
+        });
+
+  }
+  
+  
   return (
     <>
-     <FileExplorer file={data}/>
+      <FileExplorer file={file} handleCreateFile={handleCreateFile} handleDeleteNode={handleDeleteNode}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- fileExplorer/src/component/fileExplorer/index.jsx
+++ fileExplorer/src/component/fileExplorer/index.jsx
@@ -1,19 +1,103 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./index.css";
-const FileExplorer = ({ file }) => {
+const FileExplorer = ({ file, handleCreateFile,  handleDeleteNode}) => {
   const [expand, setExpand] = useState(false);
+  const [create, setCreate] = useState(false);
+  const [typeCreate,setTypeCreate]=useState(false)
+
+
+  const CREATE_FOLDER_ICON =
+    "https://cdn-icons-png.flaticon.com/512/10485/10485953.png";
+
+  const CREATE_FILE_ICON="https://cdn-icons-png.flaticon.com/512/6853/6853377.png"
+
+  const DELETE_ICON =""
+
+    const handleInput =(e,id,type)=>{
+       handleCreateFile(e,id,type)
+       setCreate(false)
+    }
+
+    const handleFileType=()=>{
+        setCreate(true)
+        setTypeCreate(false)
+    }
+    const handleFolderType=()=>{
+        setCreate(true)
+        setTypeCreate(true)
+    }
   return (
-    <div >
-      {file.type === "folder" ? (
+    <div>
+      {file.isFolder ? (
         <div className="file">
-          <div onClick={() => setExpand(!expand)} style={{cursor:"pointer"}}>📁 {file.name} </div>
-          {file.children.map(
-            data => expand && <FileExplorer key={data.id} file={data} />
+          <div style={{ cursor: "pointer" }}>
+            {/* Expand button */}
+            <span onClick={() => setExpand(!expand)}>
+              {expand ? "🔽" : "▶️"}
+            </span>{" "}
+            {file.name}{" "}
+
+            {/* Crete File Icon */}
+            {!create && (
+              <img
+                src={CREATE_FOLDER_ICON}
+                alt="create folder icon"
+                srcSet=""
+                onClick={handleFolderType}
+                width={15}
+                height={15}
+                style={{background:"whitesmoke"}}
+                />
+              )}
+            {!create && (
+              <img
+              src={CREATE_FILE_ICON}
+              alt="create file icon"
+              srcSet=""
+              onClick={handleFileType}
+              width={15}
+              height={15}
+              style={{background:"whitesmoke",marginLeft:"5px"}}
+              />
+            )}
+
+            {/* Create File Name Input */}
+            {create && (
+              <input type="text" onBlur={e => handleInput(e, file.id,typeCreate)} />
+            )}{" "}
+             <img
+                src={DELETE_ICON}
+                alt="create folder icon"
+                srcSet=""
+                onClick={()=>handleDeleteNode(file.id)}
+                width={15}
+                height={15}
+                style={{background:"whitesmoke"}}
+                />
+          </div>
+          {file.children?.map(
+            data =>
+              expand && (
+                <FileExplorer
+                  key={data.id}
+                  file={data}
+                  handleCreateFile={handleCreateFile}
+                  handleDeleteNode={handleDeleteNode}
+                />
+              )
           )}
         </div>
       ) : (
-        <div className="page">📄 {file.name}</div>
+        <div className="page">📄 {file.name} <img
+        src={DELETE_ICON}
+        alt="create folder icon"
+        srcSet=""
+        onClick={()=>handleDeleteNode(file.id)}
+        width={15}
+        height={15}
+        style={{background:"whitesmoke"}}
+        /></div>
       )}
     </div>
   );
